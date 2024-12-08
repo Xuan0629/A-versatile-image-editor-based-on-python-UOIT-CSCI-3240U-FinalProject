5ae5accd5c7a2007e10684a20b74d4459c2f627b
Xuan0629 <xuan.zheng@ontariotechu.net>
2024-12-08 05:28:39 -0500

    CT issue fix

--- image_viewer.py
+++ image_viewer.py
@@ -54,12 +54,12 @@ def adjust_color_temperature(image, temp_value):
     image = image.astype(np.float32)
     b, g, r = cv2.split(image)
     if temp_mapped >= 0:
-        r += (temp_mapped * 2.55)
-        b -= (temp_mapped * 2.55)
+        r -= (temp_mapped * 2.55)
+        b += (temp_mapped * 2.55)
     else:
         temp_mapped = -temp_mapped
-        b += (temp_mapped * 2.55)
-        r -= (temp_mapped * 2.55)
+        b -= (temp_mapped * 2.55)
+        r += (temp_mapped * 2.55)
     r = np.clip(r, 0, 255)
     g = np.clip(g, 0, 255)
     b = np.clip(b, 0, 255)
